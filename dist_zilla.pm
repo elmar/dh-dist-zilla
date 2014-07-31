ab24c3ecd9e1144f92121b0ab9806c5ad0fc412a
Axel Beckert <abe@deuxchevaux.org>
2014-07-31 16:43:19 +0200

    Use Config::INI::Reader to parse dist.ini

--- dist_zilla.pm
+++ dist_zilla.pm
@@ -2,8 +2,12 @@
 use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
+use Config::INI::Reader;
 
-my $build_dir = 'TODO: foobar...';
+my $dist_ini  = Config::INI::Reader->read_file('dist.ini');
+my $name      = $dist_ini->{'_'}{name};
+my $version   = $dist_ini->{'_'}{version};
+my $build_dir = "$name-$version";
 
 insert_before("dh_auto_configure", "dzil build");
 
