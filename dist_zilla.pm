879f0df1d349e2387648cc8e3be1b56dc7c08059
Axel Beckert <abe@deuxchevaux.org>
2014-07-31 17:45:44 +0200

    Also call dh_auto_configure inside the dzil build generated directory

--- dist_zilla.pm
+++ dist_zilla.pm
@@ -11,9 +11,10 @@ my $build_dir = "$name-$version";
 
 insert_before("dh_auto_configure", "dh_dzil_build");
 
-add_command_options("dh_auto_build",   '-D', $build_dir);
-add_command_options("dh_auto_install", '-D', $build_dir);
-add_command_options("dh_auto_test",    '-D', $build_dir);
+add_command_options("dh_auto_configure", '-D', $build_dir);
+add_command_options("dh_auto_build",     '-D', $build_dir);
+add_command_options("dh_auto_install",   '-D', $build_dir);
+add_command_options("dh_auto_test",      '-D', $build_dir);
 
 insert_before("dh_auto_clean", "dh_dzil_clean");
 
