c85dbfc9d7061fe603c9b45811ac08a4ec666d75
Axel Beckert <abe@deuxchevaux.org>
2014-07-31 16:53:08 +0200

    Fix typo (plural vs singular)

--- dist_zilla.pm
+++ dist_zilla.pm
@@ -11,9 +11,9 @@ my $build_dir = "$name-$version";
 
 insert_before("dh_auto_configure", "dzil build");
 
-add_command_option("dh_auto_build",   "-D $build_dir");
-add_command_option("dh_auto_install", "-D $build_dir");
-add_command_option("dh_auto_test",    "-D $build_dir");
+add_command_options("dh_auto_build",   "-D $build_dir");
+add_command_options("dh_auto_install", "-D $build_dir");
+add_command_options("dh_auto_test",    "-D $build_dir");
 
 insert_before("dh_auto_clean", "dzil clean");
 
