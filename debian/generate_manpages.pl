d257c01aa59707c29014914505ab7fd7684edc13
Axel Beckert <abe@deuxchevaux.org>
2014-08-13 18:52:37 +0200

    Support wildcards in debian/manpages again

--- debian/generate_manpages.pl
+++ debian/generate_manpages.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 
+# Helper script to guess which files pod2man should use as input based
+# on the man pages listed in debian/manpages. Goal: Only specify them
+# once there, i.e. DRY.
+
 use strict;
 use warnings;
 use autodie;
@@ -12,11 +16,34 @@ my @pod2man = (qw(pod2man -c dh-dist-zilla -r), "dh-dist-zilla v$version");
 open(my $dmp, '<', $list_of_manpages);
 while (my $line = <$dmp>) {
     chomp($line);
+
+    # Expect man page section as file suffix
     my ($name, $section) = split(/\./, $line);
 
-    unless (-e $name) {
-        $name .= '.pod';
+    # Expand potential wildcards and check for existence
+    my @names = get_names($name);
+
+    # If there was no initial match, try appending ".pod"
+    unless (@names) {
+        my $checkname = "$name.pod";
+        @names = get_names($checkname);
     }
 
-    system(@pod2man, "--section=$section", $name, $line);
+    # Assert non-empty list
+    die "No file found for '$line'" unless (@names);
+
+    foreach my $name (@names) {
+        # Remove .pod suffix from final name again
+        my $man_page_name = $name;
+        $man_page_name =~ s/\.pod$//;
+
+        # Finally call pod2man
+        system(@pod2man, "--section=$section",
+               $name, "$man_page_name.$section");
+    }
+}
+close($dmp);
+
+sub get_names {
+    return grep { -e } map { glob } @_;
 }
