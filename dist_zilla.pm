704a8365c38e8a8e4b09ec26610b004a28edb5ea
Manfred Stock <mstock@cpan.org>
2014-09-19 20:45:50 +0200

    Don't parse dist.ini to get distribution name and version
    There are Dist::Zilla plugins like AutoVersion or Git::NextVersion which
    make it possible to omit the version from dist.ini. Because of this, there
    were situations where the paths constructed from this (potentially missing)
    information in dist.ini pointed to inexistent directories. These issues
    can be avoided by passing the --in parameter to 'dzil build', which makes
    it put the output into a known directory which can then be used.
    
    For this known directory, I've chosen .build/debian-build since this will
    also be removed when doing a 'dzil clean'.

--- dist_zilla.pm
+++ dist_zilla.pm
@@ -2,12 +2,8 @@
 use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
-use Config::INI::Reader;
 
-my $dist_ini  = Config::INI::Reader->read_file('dist.ini');
-my $name      = $dist_ini->{'_'}{name};
-my $version   = $dist_ini->{'_'}{version};
-my $build_dir = "$name-$version";
+my $build_dir = ".build/debian-build";
 
 insert_before("dh_auto_configure", "dh_dzil_build");
 
